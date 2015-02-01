6e74acff33610395be1f1c1239b56ff5433c11b3
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-02-01 15:59:00 +0100

    More flexible wait action

--- action.go
+++ action.go
@@ -10,21 +10,27 @@ func (a ActionFunc) Do(o *Object, t clock.Time) {
 
 // Wait pauses the display of the current object
 type Wait struct {
+	// Next contains the next action.
+	Next Action
+	// Time before Next is triggered.
 	Until clock.Time
-	Next  Action
+	// DeadDuring represents the state of the object during wait.
+	DeadDuring bool
+	// DeadAfter is affected to the object at the end of the wait.
+	DeadAfter bool
 }
 
 func (w Wait) Do(o *Object, t clock.Time) {
 	if o.Time == 0 {
 		o.Time = t
-		o.Dead = true
+		o.Dead = w.DeadDuring
 		return
 	}
 	if t > o.Time+w.Until {
 		// Once the time is elapsed,
 		// start the next Action
 		o.Time = 0
-		o.Dead = false
+		o.Dead = w.DeadAfter
 		o.Action = w.Next
 	}
 }
