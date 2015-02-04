3e10924bff39469499a928c0c18eef6ff3a2562c
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-02-04 18:00:43 +0100

    texture things

---
+++ texture.go
@@ -0,0 +1,50 @@
+package fsm
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"log"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/sprite"
+)
+
+const (
+	texBg = iota
+)
+
+// MustLoadTexture loads a texture from a file or panics.
+func MustLoadTexture(eng sprite.Engine, file string) sprite.Texture {
+	f, err := app.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := eng.LoadTexture(img)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
+}
+
+// SubTex returns a new sprite.SubTex cut from a sprite.Texture.
+func SubTex(t sprite.Texture, x, y, w, h int) sprite.SubTex {
+	return sprite.SubTex{t, image.Rect(x, y, w, h)}
+}
+
+// MustLoadColorTexture creates a sprite.SubTex from a standard color.Color or panics.
+func MustLoadColorTexture(eng sprite.Engine, c color.Color, width, height int) sprite.SubTex {
+	img := image.Rect(0, 0, width, height)
+	m := image.NewRGBA(img)
+	draw.Draw(m, m.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
+	t, err := eng.LoadTexture(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return sprite.SubTex{t, img}
+}
